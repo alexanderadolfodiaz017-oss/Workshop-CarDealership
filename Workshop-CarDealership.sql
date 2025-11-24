aee86c0922a47a16bb87919311988b088b0da955
Alexander Diaz <adiaz1@my.yearupunited.org>
2025-11-24 03:07:13 -0800

    Step 2: Added vehicles and dealership table scripts

--- Workshop-CarDealership.sql
+++ Workshop-CarDealership.sql
@@ -1,3 +1,9 @@
-DROP DATABASE IF EXISTS my_cardatabase;
-CREATE DATABASE my_cardatabase;
-USE my_cardatabase;
+CREATE TABLE vehicles (
+    VIN VARCHAR(20) PRIMARY KEY,
+    make VARCHAR(50),
+    model VARCHAR(50),
+    year INT,
+    color VARCHAR(30),
+    price DECIMAL(10,2),
+    sold BOOLEAN
+);
