9e7d9a4c6ecacd0fe4c39f7ba5330bbd944cc992
Alexander Diaz <adiaz1@my.yearupunited.org>
2025-11-24 03:27:20 -0800

    Updated SQL tables and dealership/vehicle schema for workshop

--- Workshop-CarDealership.sql
+++
@@ -1,9 +0,0 @@
-CREATE TABLE vehicles (
-    VIN VARCHAR(20) PRIMARY KEY,
-    make VARCHAR(50),
-    model VARCHAR(50),
-    year INT,
-    color VARCHAR(30),
-    price DECIMAL(10,2),
-    sold BOOLEAN
-);
